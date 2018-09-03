d0c0fa41a0dcedd0893bb2c9b957c6bd779f9993
SistemaGlobal <tayronewesleymartins@gmail.com>
2018-09-03 16:09:41 -0300

    atualização

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Laravel-Web-Service
                 </div>                
             </div>
         </div>
