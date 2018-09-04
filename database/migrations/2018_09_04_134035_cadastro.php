06dff27d364c2329ead25f6b55f5beaab31064bd
SistemaGlobal <tayronewesleymartins@gmail.com>
2018-09-04 13:01:57 -0300

    Webservice Atualização

--- database/migrations/2018_09_04_134035_cadastro.php
+++ database/migrations/2018_09_04_134035_cadastro.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Cadastro extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+        //
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->increments('id')->unique();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('dt_entrega');
+            $table->string('p_partida');
+            $table->string('destino');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -30,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,24 @@
 |
 */
 
+Route::group(['prefix' => 'api','middleware' => 'cors'], function()
+{
+    Route::group(['prefix' => 'user'], function()
+    {
+        ##tipos de entrada select todos os id's
+        Route::get('', ['uses' => 'UserController@allUsers']);
+        ##tipos de entrada devolve apenas um id
+        Route::get('{id}', ['uses' => 'UserController@umUsers']);
+        ##tipos de entrada cria um novo cadastro
+        Route::post('', 'UserController@cadastroUsers');
+        ##tipos de entrada atualiza o id
+        Route::put('{id}', ['uses' => 'UserController@updateUsers']);
+        ##tipos de entrada deleta o id
+        Route::delete('{id}', ['uses' => 'UserController@deteleUser']);
+    });
+});
+#   
+##  Fim
 Route::get('/', function () {
     return view('welcome');
 });
